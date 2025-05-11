14b95a47e5c158afa0bd7ddbdcf46f7a452e3056
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2025-05-11 22:18:43 +0200

    feat: implement RankCard and RankItem components for user ranking display

--- front/src/components/Card/Card.ts
+++ front/src/components/Card/Card.ts
@@ -1,9 +1,11 @@
 import { Component, ComponentProps } from '../../utils/component';
 
-interface CardProps extends ComponentProps {
+export interface CardProps extends ComponentProps {
+  id: string;
   title: string;
-  content?: string;
-  footer?: Component;
+  width?: string;
+  height?: string;
+  content?: Component;
 }
 
 export class Card extends Component {
@@ -12,20 +14,56 @@ export class Card extends Component {
   constructor(props: CardProps) {
     super(props);
     this.props = props;
-    this.template = `<div class="bg-white rounded-lg shadow-md p-6">
-  <h3 class="text-xl font-bold mb-2">{{ title }}</h3>
-  <div class="card-content">{{ content }}</div>
-  <div class="card-footer mt-4"></div>
-</div>`;
+    this.template = this.renderTemplate();
   }
 
+  renderTemplate() {
+
+    return `
+        <div class="backdrop-blur-3xl bg-opacity-15 bg-[#1D1F2B] ${this.props.width} ${this.props.height} border border-white border-opacity-15 rounded-2xl shadow-lg flex flex-col overflow-hidden">
+        <!-- Header del chat -->	
+        <div id="${this.props.id}-header" class="relative flex justify-center items-center space-x-2 px-4 py-2 text-center text-white text-sm">
+        ${this.props.title}
+        </div>
+        <!-- Divider -->
+        <hr id="${this.props.id}-divider"class="w-full border-t border-white border-opacity-15" />
+
+          <div id="${this.props.id}-body" class=" flex-1 overflow-y-auto text-sm  space-y-2">
+          </div>
+
+        </div>
+
+    `}
   protected initEvents(): void {
-    // Si hay un componente footer, lo renderizamos dentro del card
-    if (this.props.footer && this.element) {
-      const footerContainer = this.element.querySelector('.card-footer');
-      if (footerContainer) {
-        footerContainer.appendChild(this.props.footer.render());
-      }
+    const body = this.element?.querySelector(`#${this.props.id}-body`) as HTMLElement;
+    if (!body) return;
+    if (this.props.content) {
+      body.appendChild(this.props.content.render());
     }
   }
+}
+
+
+
+interface ChartCardProps extends CardProps {
+
+}
+
+export class ChartCard extends Card {
+  protected body: string;
+  protected props: ChartCardProps;
+
+  constructor(props: ChartCardProps) {
+    super(props);
+      this.props =props;
+    this.body = this.renderBody();
+  }
+
+  renderBody(): string {
+    // Aquí puedes crear el cuerpo del card, por ejemplo, un gráfico o una lista
+    return `
+    <div class="p-4">Este es el cuerpo del card</div>
+    `;
+  }
+
 }
